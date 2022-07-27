80136bb2bd81385a5b5e0e000a74f105c22dd9e0
Guilherme Lourenço <76043248+Lourencobrah@users.noreply.github.com>
2022-07-27 10:45:38 -0300

    Referenciando o local onde está o arquivo main

--- openMain.py
+++ openMain.py
@@ -1,4 +1,4 @@
-file = open("C:\Users\guilherme.lourenco\Documents\Guilherme Lourenço\Python\Cotacao\rpa_cotacao\main.py", "r")
+file = open("C:\* LOCAL DO AONDE ESTÁ O MAIN.PY *\main.py", "r")
 content = file.read()
 print(content)
 file.close()
