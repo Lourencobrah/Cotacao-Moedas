ddde3c8fe38517f6e310fe926a6a71f1e50ba035
Guilherme Vicente Lourenço <lourencospecter@gmail.com>
2022-07-27 10:43:22 -0300

    Primeiro commit do projeto

---
+++ openMain.py
@@ -0,0 +1,4 @@
+file = open("C:\Users\guilherme.lourenco\Documents\Guilherme Lourenço\Python\Cotacao\rpa_cotacao\main.py", "r")
+content = file.read()
+print(content)
+file.close()
--- rpa_cotacao/openMain - Copia.py
+++
@@ -1,4 +0,0 @@
-file = open("C:\Users\guilherme.lourenco\Documents\Guilherme Lourenço\Python\Cotacao\main.py", "r")
-content = file.read()
-print(content)
-file.close()
